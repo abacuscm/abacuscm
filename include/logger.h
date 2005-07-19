4c841ebfcd83e0136f339a017dca20b67394c397
jkroon <jkroon@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2005-07-19 18:59:26 +0000

    Fix typo in logger.h: implement -> implemented
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@14 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- include/logger.h
+++ include/logger.h
@@ -7,6 +7,6 @@
 void log(int priority, const char* format, ...) __attribute__ ((format (printf, 2, 3)));
 void lerror(const char* prefix);
 
-#define NOT_IMPLEMENTED() log(LOG_DEBUG, "%s (%s:%d) not implementd.", __PRETTY_FUNCTION__, __FILE__, __LINE__)
+#define NOT_IMPLEMENTED() log(LOG_DEBUG, "%s (%s:%d) not implemented.", __PRETTY_FUNCTION__, __FILE__, __LINE__)
 
 #endif
