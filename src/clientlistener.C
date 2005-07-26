0bacd8d593789fd7f143a9a573a065b8405dff10
jkroon <jkroon@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2005-07-26 14:59:58 +0000

    Added some handling for bind() to ClientListener - still needs to obtain port number from config
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@48 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- src/clientlistener.C
+++ src/clientlistener.C
@@ -16,12 +16,16 @@ ClientListener::~ClientListener() {
 bool ClientListener::init(SocketPool *pool) {
 	int sock = socket(PF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in addr;
-
 	
 	if(sock < 0) {
 		lerror("socket");
 		return false;
 	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(7386);
+	addr.sin_addr.s_addr = INADDR_ANY;
 
 	if(bind(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
 		lerror("bind");
