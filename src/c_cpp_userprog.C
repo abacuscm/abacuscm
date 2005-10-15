71bd76dc231622819f7d186ef953dab5e818fb39
jkroon <jkroon@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2005-10-15 08:39:44 +0000

    Added libmath (-lm) to C/C++ linking
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@305 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- src/c_cpp_userprog.C
+++ src/c_cpp_userprog.C
@@ -49,6 +49,7 @@ string C_CPP_UserProg::sourceFilename(const Buffer&) {
 bool C_CPP_UserProg::compile(string infile, string compiler_log, string outdir) {
 	list<string> argv;
 	argv.push_back(compiler());
+	argv.push_back("-lm");
 	argv.push_back("-static");
 	argv.push_back("-o");
 	argv.push_back(outdir + "/" + _progname);
