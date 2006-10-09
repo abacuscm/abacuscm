5c3f0006fa863a5927e163d41b91aa1435417aea
jkroon <jkroon@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2006-10-09 17:28:38 +0000

    Ugly hack to make reloading the DB less cumbersome ... not too stable with regards to the grep'ing but it should work mostly.
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@357 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- db/reload_sql.sh
+++ db/reload_sql.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-db=$1
-[ "${db}" == "" ] && db=abacus
+if [ -n "$1" ]; then
+	[ ! -f "$1" ] && echo "USAGE: $0 [server_conf_file]" && exit -1
+	conf_file="$1"
+else
+	conf_file="$(dirname $0)/../conf/server.conf"
+	[ ! -f "${conf_file}" ] && echo "Unable to locate the config file ... please pass it as first parameter." && exit -1
+fi
 
+eval "$(cat "${conf_file}" | grep -A7 '[[]mysql]' | awk -F= '{ if(NF==2) print $1 "='"'"'" $2 "'"'"'"; }')"
+
+(
 echo "DROP DATABASE ${db};"
 echo "CREATE DATABASE ${db};"
 echo "\r ${db}"
 echo "$(<$(dirname $0)/structure.sql)"
+) | mysql --user="${user}" --pass="${password}" --port="${port}" --host="${host}"
