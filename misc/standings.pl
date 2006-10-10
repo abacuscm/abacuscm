97ec90a6b1999f8c6c4b849c6cbe92d44b717f35
bmerry <bmerry@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2006-10-10 08:00:11 +0000

    - Make online standings prettier and more robust - Support id->name mappings in online standings.
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@382 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- misc/standings.pl
+++ misc/standings.pl
@@ -1,9 +1,23 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -w -T
 use strict;
 use warnings;
-use CGI::Pretty qw/:standard *table/;
+use CGI::Pretty qw/:standard *table *tbody/;
 
 my $standings_path = "/home/bmerry/devel/abacuscm/standings.txt";
+my $names_path = "/home/bmerry/names.txt";
+
+my %name_map = ();
+if (open(NAMES, '<', $names_path))
+{
+	while (<NAMES>)
+	{
+		chomp;
+		my ($id, $name) = split(/\t/, $_, 2);
+		$name_map{$id} = $name;
+	}
+	close(NAMES);
+}
+
 if (!open(IN, '<', $standings_path))
 {
 	print(header(-status => 500),
@@ -17,16 +31,32 @@ if (!open(IN, '<', $standings_path))
 print(header,
 	start_html(
 		-title => "ACM ICPC South Africa",
-		-style => {-src => "icpc.css"}
+		-style => {-src => "abacuscm.css"},
+		-head => meta({-http_equiv => 'Refresh', -content => '30' })
 	),
 	h1("ACM ICPC standings"),
 	start_table({-border => undef}),
-        Tr(th([split(/\t|\n/, <IN>)])));
+        thead(Tr(th([map { escapeHTML($_) } split(/\t|\n/, <IN>)]))),
+	start_tbody);
 while (<IN>)
 {
-	print Tr(td([split /\t|\n/]));
+	chomp;
+	my @fields = split(/\t/);
+	next unless scalar(@fields);
+	if (exists($name_map{$fields[0]}))
+	{
+		$fields[0] = "$name_map{$fields[0]} ($fields[0])";
+	}
+	@fields = map { escapeHTML($_) } @fields;
+	for (@fields[1..$#fields-2])
+	{
+		s/Yes/span({-class => 'correct'}, 'Yes')/e;
+	}
+	print Tr({-class => ($. & 1 ? "odd" : "even")},
+		td([@fields]));
 }
-print(end_table,
+print(end_tbody,
+	end_table,
 	p(small("Generated at " . escapeHTML(scalar(localtime())) . ".")),
 	end_html);
 close(IN);
