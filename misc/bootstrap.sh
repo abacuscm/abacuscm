5f69867f9a9c98b3f84e600495269b59b6552563
bmerry <bmerry@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2007-08-06 12:39:05 +0000

    Add some helpful output about the noise
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@451 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- misc/bootstrap.sh
+++ misc/bootstrap.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -e
+echo "There may be some warnings. You can probably ignore them"
+echo
 libtoolize --automake --copy --ltdl
 autoreconf --install
+echo
+echo "Bootstrapping succeeded"
