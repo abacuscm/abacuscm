7d3efe3aad2a55a20c108cebc4677bc38cc931e1
bmerry <bmerry@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2009-12-29 21:24:31 +0000

    Prevent mysql from pausing the app for 5 seconds at the end
    This is a hack. It moves the call to pthread_create_key for MySQL
    shutdown handler which has the effect of switching destructor order, but
    it's not based on any standards guarantee.
    
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@583 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- src/dbmysql.C
+++ src/dbmysql.C
@@ -1399,9 +1399,9 @@ static void thread_deinit(void* initialised)
 
 static void init() __attribute__((constructor));
 static void init() {
+	pthread_key_create(&_called_thread_init, thread_deinit);
 	mysql_server_init(0, NULL, NULL);
 	DbCon::registerFunctor(MySQLFunctor);
-	pthread_key_create(&_called_thread_init, thread_deinit);
 	pthread_setspecific(_called_thread_init, MYSQL_THREAD_STATE_SERVER_INIT);
 }
 
