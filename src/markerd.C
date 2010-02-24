5a879cf80ef2e4e41cfa2af804219250acf353dc
xeer <xeer@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2010-02-24 23:25:08 +0000

    Fixes for the marker to have it attempt to reconnect when a number of things go wrong, per suggestion from Bruce.
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/branches/it_challenge@691 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- src/markerd.C
+++ src/markerd.C
@@ -45,25 +45,32 @@ int main(int argc, char **argv) {
 	for (;;) {
 		ServerConnection _server_con;
 		if(!_server_con.connect(conf["server"]["address"], conf["server"]["service"]))
-			return -1;
+			goto retry;
 
-		if(!_server_con.auth(conf["server"]["username"], conf["server"]["password"]))
-			return -1;
+		if(!_server_con.auth(conf["server"]["username"], conf["server"]["password"])) {
+			// Perhaps the server hasn't been populated with the correct user credentials
+			// yet. Let's sleep and then have another go...
+			goto disconnect;
+		}
 
 		log(LOG_DEBUG, "Connected to server!");
 
 		_server_con.registerEventCallback("mark", mark_request, NULL);
 		_server_con.registerEventCallback("close", server_close, NULL);
 
-		if(!_server_con.becomeMarker())
-			return -1;
+		if(!_server_con.becomeMarker()) {
+			// More severe than failing to login, but again perhaps there's just a glitch
+			// on the server with our user setup. Sleep and have another go...
+			log(LOG_DEBUG, "Failed to become a marker");
+			goto disconnect;
+		}
 
 		MarkRequest *mr;
 		while((mr = _mark_requests.dequeue()) != NULL) {
 			AttributeMap attrs;
 			if(!_server_con.getProblemAttributes(mr->prob_id, attrs)) {
 				log(LOG_CRIT, "Cannot function if we cannot retrieve problem attributes!");
-				return -1;
+				goto disconnect;
 			}
 
 			string problem_type = attrs["prob_type"];
@@ -71,7 +78,7 @@ int main(int argc, char **argv) {
 
 			if (!marker) {
 				log(LOG_CRIT, "Error locating marker for problem type '%s'.", attrs["prob_type"].c_str());
-				return -1;
+				goto disconnect;
 			}
 
 			marker->setServerCon(&_server_con);
@@ -81,15 +88,19 @@ int main(int argc, char **argv) {
 			marker->mark();
 			if(!marker->submitResult()) {
 				log(LOG_CRIT, "Great, failed to upload mark, bailing entirely ...");
-				return -1;
+				goto disconnect;
 			}
 
 			delete marker;
 		}
 
-		log(LOG_INFO, "Server has disconnected. Sleeping for 10 seconds.");
+		log(LOG_INFO, "Server has disconnected");
 
+		disconnect:;
 		_server_con.disconnect();
+
+		retry:;
+		log(LOG_INFO, "Sleeping for 10 seconds");
 		sleep(10);
 	}
 
