527eddad1b0f9ee33a3ce0a2f3194a2e1654b96a
jkroon <jkroon@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2010-10-13 08:44:42 +0000

    Remove some further autoconf artifacts.
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/trunk@735 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- misc/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-echo "There may be some warnings. You can probably ignore them"
-echo
-misc/qtdeps.sh
-libtoolize --automake --copy --ltdl
-autoreconf --install
-echo
-echo "Bootstrapping succeeded"
