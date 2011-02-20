9afbab681db32ac61df518c164f1c34e2471b91e
bmerry <bmerry@ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f>
2011-02-20 17:37:22 +0000

    Change label on stderr to that stdout and expected output are next to each other
    git-svn-id: https://abacuscm.svn.sourceforge.net/svnroot/abacuscm/branches/it_challenge@804 ac3b2d11-31fc-0310-b7bc-aecf6fe4fc4f

--- src/testcaseproblemmarker.C
+++ src/testcaseproblemmarker.C
@@ -41,7 +41,7 @@ void TestCaseProblemMarker::mark_compiled() {
 				errfile.c_str(),
 				runfile.c_str()) == 0) {
 
-		addResultFile("Team's stderr", errfile, 64 * 1024);
+		addResultFile("Team's error log", errfile, 64 * 1024);
 		// parse the runinfo output
 		if (_run_info)
 			delete _run_info;
