f256e9dc842502f3cc75110c30f14cf4a8ab8a8c
Bruce Merry <bmerry@gmail.com>
2013-08-07 11:55:52 +0200

    Add script to simplify adding multiple users

---
+++ misc/addusers.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+import re
+import time
+
+cmd = 'bin/adduser'
+
+def usage(err = None):
+    if err is not None:
+        print >>sys.stderr, err
+    print >>sys.stderr, 'Usage: %s userfile' % (os.path.basename(sys.argv[0]))
+    sys.exit(1)
+
+def get_admin_password(server_config):
+    with open(server_config, 'r') as f:
+        for line in f:
+            match = re.match('\s*admin_password\s*=\s*(\S+)', line)
+            if match:
+                return match.group(1)
+    print >>sys.stderr, 'Admin password not found in', server_config
+    sys.exit(1)
+
+if len(sys.argv) != 2:
+    usage()
+
+config = "abacus.conf"
+server_config = "conf/server.conf"
+password = get_admin_password(server_config)
+username = "admin"
+userfile = sys.argv[1]
+if not os.path.isfile(config):
+    usage('%s is not a file' % config)
+if not os.path.isfile(userfile):
+    usage('%s is not a file' % userfile)
+
+if not os.path.isfile(cmd):
+    usage('Please run from the abacus top-level directory')
+
+users = []
+f = file(userfile, 'r')
+for line in f:
+    line = line.rstrip()
+    matches = re.match('^([a-z0-9_]+) ([a-zA-Z0-9]+) ([a-z]+) (.+)$', line)
+    if matches:
+        new_username = matches.group(1)
+        new_password = matches.group(2)
+        new_type     = matches.group(3)
+        new_realname = matches.group(4)
+
+        argv = [cmd, config, username, password, new_username, new_realname, new_password, new_type]
+        print 'Adding %s => %s identified by %s as type %s' % (new_username, new_realname, new_password, new_type)
+        ret = subprocess.call(argv)
+        if ret != 0:
+            sys.exit(ret)
+    else:
+        print 'Skipping line \'%s\' which is not in a format that we expect.' % line
