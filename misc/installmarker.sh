00e4a93220dc52db2e15f9ed0627c6ff14b9e9cc
Jaco Kroon <jaco@uls.co.za>
2014-10-03 14:25:02 +0200

    Turns out Ubuntu (at least) doesn't like adding a user unless the group exists first.

--- misc/installmarker.sh
+++ misc/installmarker.sh
@@ -5,6 +5,7 @@ mkdir -p /etc/abacus
 cp bin/markerd /usr/local/bin
 cp bin/runlimit /usr/local/bin
 cp conf/marker.conf.sample /etc/abacus/marker.conf
+groupadd markerd
 useradd -g markerd -m -k /etc/skel -s /bin/bash markerd
 chown root:markerd /usr/local/bin/runlimit
 chmod 4750 /usr/local/bin/runlimit
