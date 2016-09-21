5f4ed50d835ce4165a5afd314ccab5eac58b377a
Bruce Merry <bmerry@ska.ac.za>
2016-09-21 14:10:43 +0200

    Add lots of debug logging to the run script
    It's disabled by default, but I left them in for debugging future
    problems.

--- docker/run.py
+++ docker/run.py
@@ -16,6 +16,7 @@ import warnings
 import functools
 import random
 import pickle
+import logging
 import dbm.ndbm
 from contextlib import contextmanager, closing
 
@@ -60,6 +61,7 @@ def run_once(func):
     return wrapper
 
 def abacustool(*args):
+    logging.debug('Running abacustool [%s]', args)
     subprocess.check_call(['/usr/bin/abacustool',
         '-c', ADMIN_CONF, '-s', SERVER_CONF] + list(args))
 
@@ -75,20 +77,24 @@ def mkdir_p(path, mode=0o0777):
 
 def create_key(out, args):
     """Create x509 private key"""
+    logging.debug('Creating x509 private key')
     subprocess.check_call(['openssl', 'genrsa', '-out', out, '4096'])
 
 def create_self_cert(subj, key, out, args):
     """Create a self-signed certificate"""
+    logging.debug('Creating self-signed certificate')
     subprocess.check_call([
         'openssl', 'req', '-new', '-x509', '-key', key,
         '-days', str(args.days), '-subj', subj, '-out', out])
 
 def create_csr(subj, key, out, args):
     """Create a certificate signing request"""
+    logging.debug('Creating certificate signing request')
     subprocess.check_call([
         'openssl', 'req', '-new', '-key', key, '-subj', subj, '-out', out])
 
 def create_cert(csr, ca_key, ca_cert, ca_serial, out, args):
+    logging.debug('Creating certificate')
     subprocess.check_call([
         'openssl', 'x509', '-req', '-in', csr,
         '-CAcreateserial', '-days', str(args.days),
@@ -96,12 +102,14 @@ def create_cert(csr, ca_key, ca_cert, ca_serial, out, args):
         '-out', out])
 
 def create_jks_truststore(cert, truststore, alias, password):
+    logging.debug('Creating JKS trust store')
     subprocess.check_call([
         'keytool', '-importcert', '-alias', 'abacuscert',
         '-file', cert, '-keystore', truststore, '-storepass', password,
         '-noprompt'])
 
 def create_jks_keystore(cert, key, keystore, password):
+    logging.debug('Creating JKS key store')
     with umask(0o077):
         (handle, pkcs12) = tempfile.mkstemp('.pkcs12')
         try:
@@ -117,15 +125,18 @@ def create_jks_keystore(cert, key, keystore, password):
             os.unlink(pkcs12)
 
 def is_mysql_running():
+    logging.debug('Checking whether mysql is running')
     with open('/dev/null', 'w') as devnull:
         ret = subprocess.call(['/usr/bin/mysqladmin', 'ping'],
                 stdin=devnull, stdout=devnull, stderr=devnull)
+    logging.debug('MySQL %s running', 'is' if ret == 0 else 'is not')
     return ret == 0
 
 @contextmanager
 def run_mysql():
     """Start mysql server and wait for it to be ready; on context
     exit, shut down the server again."""
+    logging.debug('Starting MySQL')
     proc = subprocess.Popen(['/usr/bin/mysqld_safe', '--skip-syslog'],
             stdin=subprocess.PIPE, close_fds=True, cwd='/usr')
     running = False
@@ -137,14 +148,18 @@ def run_mysql():
     if not running:
         proc.terminate()
         raise RuntimeError('mysqld did not start successfully')
+    logging.debug('MySQL running')
 
     yield
 
+    logging.debug('Stopping MySQL')
     subprocess.check_call(['/usr/bin/mysqladmin', 'shutdown'])
     proc.wait()
+    logging.debug('MySQL terminated')
 
 def is_abacusd_running():
     """Log in as admin to check whether abacusd is running"""
+    logging.debug('Check whether abacusd is running')
     proc = subprocess.Popen(['/usr/bin/batch', ADMIN_CONF],
             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cp = inichange.merge([SERVER_CONF])
@@ -157,6 +172,7 @@ def is_abacusd_running():
 
             '''.format(password=cp['initialisation']['admin_password']))
     (stdout, stderr) = proc.communicate(bytes(commands, 'utf-8'))
+    logging.debug('abacusd %s running', 'is' if proc.returncode == 0 else 'is not')
     return proc.returncode == 0
 
 @contextmanager
@@ -175,13 +191,17 @@ def run_abacusd():
     if not running:
         proc.terminate()
         raise RuntimeError('abacusd did not start successfully')
+    logging.debug('abacusd running')
 
     yield
 
+    logging.debug('Stopping abacusd')
     proc.send_signal(signal.SIGINT)
     proc.wait()
+    logging.debug('abacusd shut down')
 
 def exec_sql(commands):
+    logging.debug('Executing SQL command')
     proc = subprocess.Popen(['/usr/bin/mysql', '-u', 'root', '--batch'],
             stdin=subprocess.PIPE)
     proc.communicate(bytes(commands, 'utf-8'))
@@ -191,6 +211,7 @@ def exec_sql(commands):
 def make_abacus_certs(args):
     """If the abacus server key and CA do not exist, create them."""
     if not os.path.isdir(ABACUS_CERT_DIR):
+        logging.debug('Creating abacus certificates')
         tmp_cert_dir = os.path.join(CONF_DIR, 'abacus-certs.tmp')
         if os.path.isdir(tmp_cert_dir):
             shutil.rmtree(tmp_cert_dir)
@@ -212,6 +233,7 @@ def make_abacus_certs(args):
 def make_jetty_certs(args):
     """If no certificate exists for Jetty, create a self-signed one"""
     if not os.path.isdir(JETTY_CERT_DIR):
+        logging.debug('Creating Jetty certificates')
         if args.server is None:
             msg = textwrap.dedent("""\
                 No Jetty certificate found and --server not specified.
@@ -237,6 +259,7 @@ def random_password():
 
 def make_password(pwfile):
     if not os.path.exists(pwfile):
+        logging.debug('Creating password in %s', pwfile)
         r = random.SystemRandom()
         password = ''.join([r.choice(string.ascii_letters) for i in range(16)])
         with open(pwfile, 'w', 0o600) as f:
@@ -245,6 +268,7 @@ def make_password(pwfile):
 def make_mysql(args):
     """Initialise mysql db if it does not exist"""
     if not os.path.isdir(os.path.join(MYSQL_DIR, 'db', 'mysql')):
+        logging.debug('Initialising MySQL')
         fix_permission(MYSQL_DIR, 'mysql', 'mysql')
         commands = [textwrap.dedent("""\
             create database abacus;
@@ -257,9 +281,11 @@ def make_mysql(args):
         with open(init_file, 'w') as init:
             init.write(''.join(commands))
         subprocess.check_call(['mysqld', '--initialize-insecure', '--init-file', init_file])
+        logging.debug('MySQL initialised')
 
 def make_server_conf(args):
     """Merges override configuration with preconfigured values"""
+    logging.debug('Creating server config')
     filenames = [os.path.join(SRC_DIR, 'docker', 'server.conf')]
     for path in [os.path.join(CONF_DIR, 'abacus'), CONTEST_DIR]:
         filename = os.path.join(path, 'server.conf.override')
@@ -272,6 +298,7 @@ def make_server_conf(args):
 
 def make_marker_conf(args):
     """Merges override configuration for a marker with preconfigured values"""
+    logging.debug('Creating marker config')
     filenames = [os.path.join(SRC_DIR, 'docker', 'marker.conf')]
     override_file = os.path.join(CONF_DIR, 'abacus', 'marker.conf.override')
     overrides = []
@@ -288,6 +315,7 @@ def make_server_crypto(args):
     """Build the keystore from the abacus certificate, and generate IVs. This
     is done every time, because it is not stored persistently.
     """
+    logging.debug('Preparing server crypto')
     if os.path.exists(KEYSTORE):
         os.remove(KEYSTORE)
     if os.path.exists(TRUSTSTORE):
@@ -334,6 +362,7 @@ def add_standings_user():
     add_user('standings', 'Standings bot', password, 'contestant')
 
 def adjust_https_port(args):
+    logging.debug('Adjusting Jetty to use port %d', args.https_port)
     subprocess.check_call([
         'sed', '-i',
         's!<Set name="confidentialPort">[0-9]*</Set>' + \
@@ -341,10 +370,12 @@ def adjust_https_port(args):
         os.path.join(JETTY_DIR, 'jetty.xml')])
 
 def suid_runlimit(args):
+    logging.debug('Setting permissions on runlimit')
     shutil.chown('/usr/bin/runlimit', 'root', 'abacus')
     os.chmod('/usr/bin/runlimit', 0o4550)
 
 def install_supervisor_conf(basename, args):
+    logging.debug('Installing supervisord config')
     shutil.copy(os.path.join(SRC_DIR, 'docker', basename),
             os.path.join('/etc/supervisor/conf.d', basename))
 
@@ -378,6 +409,7 @@ def fix_permissions(args):
     fix_permission(STANDINGS_DIR, 'abacus', 'abacus', 'a+rX')
 
 def exec_supervisor(args):
+    logging.debug('Launching supervisord')
     os.execv('/usr/bin/supervisord',
             ['/usr/bin/supervisord', '-n', '-c', '/etc/supervisor/supervisord.conf'])
 
@@ -416,6 +448,7 @@ def run_marker(args):
     exec_supervisor(args)
 
 def main():
+    logging.basicConfig(level='INFO')
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
